e8fe2b31d77d617ac62b43b7f73dd9b6e466b903
Jacob McKiernan <138262144+j-mckiern@users.noreply.github.com>
2024-04-26 17:16:45 -0400

    Update script.js
    Added merge button function

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ document.getElementById('generate-array').addEventListener('click',function(){
     generateArray();
 });
 
-/*
-
 document.getElementById('merge').addEventListener('click',function(){
-    
+    mergeSort(array, 0, array.length -1);
 });
+
+/*
 document.getElementById('quick').addEventListener('click',function(){
     
 });
@@ -22,7 +22,7 @@ document.getElementById('bubble').addEventListener('click',function(){
 function generateArray(){
     arrayContainer.innerHTML = '';
     for(let i = 0; i < 50; i++){
-       let value =  Math.floor(Math.random() * 100) + 10;
+       let value =  (Math.floor(Math.random() * 100) + 10)/150;
        array[i] = value;
        const bar = document.createElement('div');
        bar.classList.add('bar');
@@ -36,8 +36,27 @@ function adjustBarHeights() {
     const maxHeight = arrayContainer.clientHeight;  
     array.forEach((value, index) => {
         const bar = arrayContainer.children[index];
-        bar.style.height = `${(maxHeight * value) / 150}px`; 
+        bar.style.height = `${(value) * maxHeight}px`;
     });
+}
+
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+function sound(dur, freq, vol){
+    let oscillator = audioCtx.createOscillator();
+    let gainNode = audioCtx.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    gainNode.gain.value = vol;
+    oscillator.frequency.value = freq;
+    oscillator.type = 'sine';
+
+    oscillator.start();
+    setTimeout(() => {
+        oscillator.stop();
+    }, dur);
 }
 
 generateArray();
